b1ab25f7a198698c30a07addc5afb5450a43faf2
bureke <sirkku.heija@gmail.com>
2018-06-14 17:19:16 +0300

    OnClickListener... exists.

--- app/src/main/java/fi/haaga_helia/sheija/plantspotting/RecyclerViewAdapter.java
+++ app/src/main/java/fi/haaga_helia/sheija/plantspotting/RecyclerViewAdapter.java
@@ -1,10 +1,14 @@
 package fi.haaga_helia.sheija.plantspotting;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -16,9 +20,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name, latinName, note;
+        public LinearLayout linearLayout;
 
+        //mitä yhdessä viewholderissa on
         public MyViewHolder(View view) {
             super(view);
+            linearLayout = view.findViewById(R.id.Linear);
             name = view.findViewById(R.id.entryName);
             latinName = view.findViewById(R.id.entryLatinName);
             note = view.findViewById(R.id.entryNote);
@@ -41,10 +48,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        //laitetaan kentiin juttuja
         Entry entry = entryList.get(position);
         holder.name.setText(entry.getName());
         holder.latinName.setText(entry.getLatinName());
         holder.note.setText(entry.getNote());
+
+        //laitetaan linearlayouttiin onclicklistener
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Click", "This is my message");
+            }
+        });
+
     }
 
     @Override
